70cfdcc4cdc4d89b1d0cc2602c9a0bcc64676df5
Eduardo Benjamin Lopez Avila <92894636+benlopez00@users.noreply.github.com>
2026-06-23 16:17:59 -0300

    clases apperror

---
+++ back/src/utils/errors.js
@@ -0,0 +1,45 @@
+// Clase base para nuestros errores controlados
+class AppError extends Error {
+	constructor(message, statusCode) {
+		super(message);
+		this.statusCode = statusCode;
+		this.isOperational = true; // Identifica que es un error previsto por nosotros
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+// Error 400 - Bad Request (Ej: Faltan campos, año inválido)
+class ValidationError extends AppError {
+	constructor(message = "Error de validación en los datos ingresados") {
+		super(message, 400);
+	}
+}
+
+// Error 401 - Unauthorized (Ej: Token inválido o no enviado)
+class UnauthorizedError extends AppError {
+	constructor(message = "Acceso denegado. No autorizado.") {
+		super(message, 401);
+	}
+}
+
+// Error 403 - Forbidden (Ej: El usuario es USER y la ruta pide ADMIN)
+class ForbiddenError extends AppError {
+	constructor(message = "No tienes permisos para realizar esta acción.") {
+		super(message, 403);
+	}
+}
+
+// Error 404 - Not Found (Ej: Película o Género no existe)
+class NotFoundError extends AppError {
+	constructor(message = "Recurso no encontrado.") {
+		super(message, 404);
+	}
+}
+
+module.exports = {
+	AppError,
+	ValidationError,
+	UnauthorizedError,
+	ForbiddenError,
+	NotFoundError,
+};
